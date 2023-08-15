fab7f4ad3dfcfab5cf923d47bd40230aefa3ce9a
Pkalinin <pal.kalin@yandex.ru>
2023-08-15 15:35:38 +0300

    add task2.1

--- main.go
+++ main.go
@@ -10,3 +10,17 @@ func main() {
 	*a = 2
 	fmt.Print(b, "\n")
 }
+package main
+
+import "fmt"
+
+func main() {
+
+	var b int = 1
+	var a *int    
+	a = &b
+	fmt.Println(*a) 
+	*a = 2
+	fmt.Print(b, "\n")
+
+}
