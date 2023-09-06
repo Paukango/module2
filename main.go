7c6a6c778cbe40a41eb0d54cd2f8b62b52f6d807
Pkalinin <pal.kalin@yandex.ru>
2023-09-06 15:25:38 +0300

    test

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,22 +14,27 @@ func main() {
 	if err != nil {
 		fmt.Println("ошибка открытия файла")
 	}
-	defer fileOpen.Close()
-	outFile, err := os.Create("data/out.txt")
-	if err != nil {
-		fmt.Println("ошибка создания файла")
-	}
+	defer fileOpen.Close().Error()
+
 	var i int
+	buffer := make([]byte, 20)
 	scanFile := bufio.NewScanner(fileOpen)
+	reader := strings.NewReader(scanFile.Text())
+
 	for scanFile.Scan() {
-		reader := scanFile.Text()
 		i++
+		_, err := reader.Read(buffer)
+		if err == io.EOF {
+
+			break
+		}
+		if err != nil {
+			fmt.Printf("Ошибка чтения: %v\n", err)
+			break
+		}
+
 		fmt.Println(reader)
 
-		if reader != "" {
-			outFile.WriteString(scanFile.Text())
-			outFile.WriteString("\n")
-		}
 	}
-	fmt.Println(i)
+	fmt.Printf("Total strings:%d", i)
 }
