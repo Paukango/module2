18db4d167b93d782a70067b3a1f84035f221f84b
Pkalinin <pal.kalin@yandex.ru>
2023-09-06 12:07:41 +0300

    task8

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	fileOpen, err := os.Open("data/in.txt")
+	if err != nil {
+		fmt.Println("ошибка открытия файла")
+	}
+	defer fileOpen.Close()
+	outFile, err := os.Create("data/out.txt")
+	if err != nil {
+		fmt.Println("ошибка создания файла")
+	}
+
+	scanFile := bufio.NewScanner(fileOpen)
+	for scanFile.Scan() {
+		reader := scanFile.Text()
+		fmt.Println(reader)
+		outFile.WriteString(scanFile.Text())
+	}
+
+}
