8ca2c4c0d66c0031020bf5f09c0d656c030fa9c8
Pkalinin <pal.kalin@yandex.ru>
2023-09-12 14:19:26 +0300

    task8

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
 )
@@ -12,19 +13,25 @@ func main() {
 
 	in, err := os.Open("data/in.txt")
 	if err != nil {
-		panic("Open error")
+		log.Print("Open error")
 	}
 
 	defer func() {
-		if cerr := in.Close(); cerr != nil {
-			fmt.Println("Ошибка при закрытии файла:", cerr)
+		if err := in.Close(); err != nil {
+			log.Print("Ошибка при закрытии файла:", err)
 		}
 	}()
 
-	sum := fileLen()
+	sum, cerr := fileLen() // Обработка ошибок функции подсчета символов в файле
+	if cerr != nil && sum == 0 {
+		log.Print("Open error")
+	} else if cerr != nil && sum > 0 {
+		log.Print("Read error")
+	}
+
 	data := make([]byte, sum)
 	var j int
-	for err != io.EOF {
+	for {
 		n, err := in.Read(data)
 		str := fmt.Sprintf(string(data[:n]))
 		strSplt := strings.Split(str, "\n")
@@ -40,11 +47,14 @@ func main() {
 	}
 }
 
-// Подсчет символов в файле
-func fileLen() int {
+// Функция подсчета символов в файле
+func fileLen() (int, error) {
+	var predLen int
+	var sumLen int
 	in, err := os.Open("data/in.txt")
 	if err != nil {
-		panic("Open error")
+		log.Print("Open error")
+		return sumLen, err
 	}
 
 	defer func() {
@@ -54,13 +64,15 @@ func fileLen() int {
 	}()
 
 	scanner := bufio.NewScanner(in)
-	var predLen int
-	var sumLen int
 	for scanner.Scan() {
 		strng := scanner.Text()
 		lenStr := len(strng)
 		sumLen = sumLen + predLen + lenStr
 		predLen = lenStr
+		if err != io.EOF && err != nil {
+			log.Print("Read error")
+			return sumLen, err
+		}
 	}
-	return sumLen
+	return sumLen, err
 }
